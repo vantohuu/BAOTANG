b29dbfd12892de330fe4c43ee2ec6bf086060a2e
vantohuu <vantohuu1234567@gmail.com>
2023-04-25 14:02:00 +0700

    Add project files.

---
+++ QLVT/FormMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLVT
+{
+    public partial class FormMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
